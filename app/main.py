987348fb5db69bf8adab6174db0aa1044e5d9f4a
Meghana <meghana@192.168.1.3>
2025-07-30 23:39:43 +0530

    Pushing backend changes

--- app/main.py
+++ app/main.py
@@ -9,11 +9,20 @@ app = FastAPI()
 # ✅ CORS settings (adjust as needed for frontend port or deploy URL)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],  # if using Vite, else 8080 for Vue
+    allow_origins=["http://localhost:5173"],  # if using Vite, else 8080 for Vue
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+#also adding 5173 port
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # ✅ Create all tables
 Base.metadata.create_all(bind=engine)
